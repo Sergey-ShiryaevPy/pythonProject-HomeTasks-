432e7144bd5b2c55833c622637b8eff7c2225ffe
Sergey Shiryaev <Pochta.mail.ru>
2024-11-22 00:23:09 +0300

    + masks

---
+++ src/masks.py
@@ -0,0 +1,18 @@
+account_num = input()
+card_number_string = input()
+
+
+def get_mask_card_number(card_number: str) -> str:
+    """Функция, которая маскирует номер карты"""
+
+    return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[12:]}"
+
+
+def get_mask_account(account_number: str) -> str:
+    """Функция, которая маскирует номер счета"""
+
+    return f"**{account_number[-4:]}"
+
+
+print(get_mask_card_number(card_number_string))
+print(get_mask_account(account_num))
