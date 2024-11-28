d9139c6f5b04b68b7ba820c434d2c10e895e410d
Sergey Shiryaev <Pochta.mail.ru>
2024-11-28 13:57:36 +0300

    Init

--- src/widget.py
+++ src/widget.py
@@ -1,13 +1,13 @@
-from masks import get_mask_card_number, get_mask_account
+from masks import get_mask_card_number, get_mask_account # type: ignore
 
 
 def mask_account_card(card_info: str) -> str:
-    """Функция, маскирующая информацию карты и счета"""
+    """Функция, маскирующая информацию как  карты, так и счета"""
     if "Счет" in card_info:
         return f"Счет {get_mask_card_number(card_info)}"
     else:
-        card_number = get_mask_card_number(card_info[-16:])
-        card_mask = card_info.replace(card_info[-16:],card_number)
+        card_number = get_mask_account(card_info[-16:])
+        card_mask = card_info.replace(card_info[-16:], card_number)
         return card_mask
 
 
