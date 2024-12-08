3cfc87fab13c5bae80c2bface7312caf743d8303
Sergey Shiryaev <Pochta.mail.ru>
2024-12-09 01:35:25 +0300

    sort_by_state

--- src/processing.py
+++ src/processing.py
@@ -1,4 +1,5 @@
 def filter_by_state(list_of_dict: list, chosen_state: str = "EXECUTED") -> list:
+    '''pass'''
     new_list_of_dict = []
     for dictionary in list_of_dict:
         if dict.get("state") == chosen_state:
@@ -6,5 +7,7 @@ def filter_by_state(list_of_dict: list, chosen_state: str = "EXECUTED") -> list:
     return new_list_of_dict
 
 
-def sort_by_state()
+def sort_by_state(list_of_dict: list, is_reverse: bool = True) -> list
     '''pass'''
+    sorted_list = sorted(list_of_dict, key = lambda x: x["date"],reverce=is_reverse)
+    return sorted_list
